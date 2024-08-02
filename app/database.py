fae90abc2fbfe12b782747b308614b911406730b
amiralba <amir.alba4@gmail.com>
2024-08-02 18:10:11 +0300

    fixed

--- app/database.py
+++ app/database.py
@@ -11,7 +11,6 @@ from .config import settings
 # engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
-
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
