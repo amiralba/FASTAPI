550c0908585468e6ae09c01e4b57e56d7fdc7b6f
amiralba <amir.alba4@gmail.com>
2024-08-02 17:36:11 +0300

    Fix configuration issues

--- app/config.py
+++ app/config.py
@@ -1,14 +1,16 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    database_host: str
-    database_port: str
-    database_username: str
-    database_password: str
+    access_token_expire_minutes: int
+    database_hostname: str
     database_name: str
+    database_password: str
+    database_port: int
+    database_url: str
+    database_username: str
     secret_key: str
     algorithm: str
-    access_token_expire_minutes: int
+
 
     class Config:
         env_file = ".env"
