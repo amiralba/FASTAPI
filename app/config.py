607c64fbfa744041c42bf08bd9805ecc9a3a6d24
amiralba <amir.alba4@gmail.com>
2024-08-02 17:54:12 +0300

    Fix configuration issues

--- app/config.py
+++ app/config.py
@@ -6,7 +6,6 @@ class Settings(BaseSettings):
     database_name: str
     database_password: str
     database_port: int
-    database_url: str
     database_username: str
     secret_key: str
     algorithm: str
@@ -14,11 +13,5 @@ class Settings(BaseSettings):
 
     class Config:
         env_file = ".env"
-    @property
-    def database_url(self) -> str:
-        return (
-            f"postgresql://{self.database_username}:{self.database_password}@"
-            f"{self.database_hostname}:{self.database_port}/{self.database_name}"
-        )
 
 settings = Settings()
